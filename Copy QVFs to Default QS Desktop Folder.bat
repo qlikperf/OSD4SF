c6ecf2b85665ddc1de47b92e841cabe344bef2e2
Jeff Robbins <JRS@qlik.com>
2020-03-15 17:45:11 -0400

    Signed-off-by: Jeff Robbins <JRS@qlik.com>

--- Copy QVFs to Default QS Desktop Folder.bat
+++ Copy QVFs to Default QS Desktop Folder.bat
@@ -1,3 +1,5 @@
 copy /y 	"C:\OSD4SF\1_Extract\Salesforce Extract.QVF"	%USERPROFILE%\Documents\Qlik\Sense\Apps
 copy /y 	"C:\OSD4SF\2_Transform\Salesforce Transform.QVF"   		%USERPROFILE%\Documents\Qlik\Sense\Apps
 copy /y 	"C:\OSD4SF\4_App\Salesforce Dashboard.QVF"   	%USERPROFILE%\Documents\Qlik\Sense\Apps
+
+Pause "Press any key to continue..."
